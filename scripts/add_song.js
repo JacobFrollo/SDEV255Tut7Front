b2b2193ff996c59100b596556b75d03effa528ba
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-05-05 19:10:12 -0400

    Update add_song.js

--- scripts/add_song.js
+++ scripts/add_song.js
@@ -10,7 +10,7 @@ async function addSong() {
         genre: document.querySelector("#genre").value ? document.querySelector("#genre").value.split(",") : []
     }
 
-    const response = await fetch("localhost:300/api/songs", {
+    const response = await fetch("https://flaxen-regular-countess.glitch.me/api/songs", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
