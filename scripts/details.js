8af4e83b9104a526ff3f5cbcce6de52de1b5e801
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-05-05 19:11:18 -0400

    Update details.js

--- scripts/details.js
+++ scripts/details.js
@@ -3,7 +3,7 @@ addEventListener("DOMContentLoaded", async function () {
     const songID = urlparam.get("id")
     console.log(songID)
 
-    const response = await fetch("localhost:300/api/songs" + songID)
+    const response = await fetch("https://flaxen-regular-countess.glitch.me/api/songs" + songID)
     const song = await response.json()
     console.log(song)
 
